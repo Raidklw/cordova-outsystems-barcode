13a0988b907d6a48307107107ff3d1eea057720f
Raidklw <Kevin.werner@wku.edu>
2025-10-31 11:52:19 -0500

    Update scanAndGrabFrame to use OSBarcodeAdvanced

--- www/advanced.js
+++ www/advanced.js
@@ -1,12 +1,5 @@
-
+// www/advanced.js
 var exec = require('cordova/exec');
-
-/**
- * Scan a QR/Barcode and automatically snapshot the same frame to JPEG.
- * @param {Object} opts - { jpegQuality?: number (default 80), facingBack?: boolean (default true) }
- * @param {(res: {scanText:string, format:string, imageContentBase64:string})=>void} success
- * @param {(err:any)=>void} error
- */
 exports.scanAndGrabFrame = function (opts, success, error) {
-  exec(success, error, 'OSBarcode', 'scanAndGrabFrame', [opts || {}]);
+  exec(success, error, 'OSBarcodeAdvanced', 'scanAndGrabFrame', [opts || {}]);
 };
